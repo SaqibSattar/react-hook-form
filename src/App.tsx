d98a0655df2421f9d4ee484afae963ae5b395403
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-09 11:40:46 +0400

    Yup integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { YouTubeForm } from "./components/YoutubeForm";
+import { YupYouTubeForm } from "./components/YupYoutubeForm";
 
 function App() {
   return (
     <>
-      <YouTubeForm />
+      {/* <YouTubeForm /> */}
+      <YupYouTubeForm />
     </>
   );
 }
